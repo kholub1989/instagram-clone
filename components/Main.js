9bea598d3218e3d49fa7a75649b0cb1cd8569fc2
Krisztian Holub <kholub1989@gmail.com>
2021-02-12 09:05:02 -0500

    add Add and Profile screen, costumizing the buttom tab nav

--- components/Main.js
+++ components/Main.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { fetchUser } from "../redux/actions/index";
 
 import FeedScreen from "./main/Feed";
+import AddScreen from "./main/Add";
+import ProfileScreen from "./main/Profile";
 
 const Tab = createBottomTabNavigator();
 
@@ -17,7 +20,41 @@ export class Main extends Component {
   render() {
     return (
       <Tab.Navigator>
-        <Tab.Screen name="Feed" component={FeedScreen} />
+        <Tab.Screen
+          name="Feed"
+          component={FeedScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="home" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Add"
+          component={AddScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name="plus-box"
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name="account-circle"
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+        />
       </Tab.Navigator>
     );
   }
