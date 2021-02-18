00bad4621edd6abe2ae99a434be2d9967fc25b23
Krisztian Holub <kholub1989@gmail.com>
2021-02-18 12:35:01 -0500

    create update DB with Follow/Unfollow func

--- components/main/Profile.js
+++ components/main/Profile.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, View, Text, Image, FlatList } from "react-native";
+import { StyleSheet, View, Text, Image, FlatList, Button } from "react-native";
 import firebase from "firebase";
 require("firebase/firestore");
 
@@ -8,6 +8,7 @@ import { connect } from "react-redux";
 function Profile(props) {
   const [userPosts, setUserPosts] = useState([]);
   const [user, setUser] = useState(null);
+  const [following, setFollowing] = useState(false);
 
   useEffect(() => {
     const { currentUser, posts } = props;
@@ -47,6 +48,26 @@ function Profile(props) {
     }
   }, [props.route.params.uid]);
 
+  const onFollow = () => {
+    firebase
+      .firestore()
+      .collection("following")
+      .doc(firebase.auth().currentUser.uid)
+      .collection("userFollowing")
+      .doc(props.route.params.uid)
+      .set({});
+  };
+
+  const onUnfollow = () => {
+    firebase
+      .firestore()
+      .collection("following")
+      .doc(firebase.auth().currentUser.uid)
+      .collection("userFollowing")
+      .doc(props.route.params.uid)
+      .delete();
+  };
+
   if (user === null) {
     return <View />;
   }
@@ -55,6 +76,16 @@ function Profile(props) {
       <View style={styles.containerInfo}>
         <Text>{user.name}</Text>
         <Text>{user.email}</Text>
+
+        {props.route.params.uid !== firebase.auth().currentUser.uid ? (
+          <View>
+            {following ? (
+              <Button title="Following" onPress={() => onUnfollow()} />
+            ) : (
+              <Button title="Follow" onPress={() => onFollow()} />
+            )}
+          </View>
+        ) : null}
       </View>
       <View style={styles.containerGallery}>
         <FlatList
