1fb23a9484984ea4ccd33b0d40c48081df1fbd3f
Krisztian Holub <kholub1989@gmail.com>
2021-02-25 17:55:38 -0500

    implement clearData reducers to clear a redux state

--- redux/reducers/users.js
+++ redux/reducers/users.js
@@ -1,6 +1,7 @@
 import {
   USERS_DATA_STATE_CHANGE,
   USERS_POSTS_STATE_CHANGE,
+  CLEAR_DATA,
 } from "../constants";
 
 const initialState = {
@@ -22,6 +23,11 @@ export const users = (state = initialState, action) => {
         users: state.users.map((user) =>
           user.uid === action.uid ? { ...user, posts: action.posts } : user
         ),
+      };
+    case CLEAR_DATA:
+      return {
+        users: [],
+        usersLoaded: 0,
       };
     default:
       return state;
