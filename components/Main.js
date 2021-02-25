9d26dfe39b3839eb5dd9f356ff17483cb0b5c35c
Krisztian Holub <kholub1989@gmail.com>
2021-02-25 17:56:54 -0500

    call clearData functionality

--- components/Main.js
+++ components/Main.js
@@ -9,6 +9,7 @@ import {
   fetchUser,
   fetchUserPosts,
   fetchUserFollowing,
+  clearData,
 } from "../redux/actions/index";
 
 import FeedScreen from "./main/Feed";
@@ -23,6 +24,7 @@ const EmptyScreen = () => {
 
 export class Main extends Component {
   componentDidMount() {
+    this.props.clearData();
     this.props.fetchUser();
     this.props.fetchUserPosts();
     this.props.fetchUserFollowing();
@@ -96,7 +98,7 @@ const mapStateToProps = (store) => ({
 
 const mapDispatchProps = (dispatch) =>
   bindActionCreators(
-    { fetchUser, fetchUserPosts, fetchUserFollowing },
+    { fetchUser, fetchUserPosts, fetchUserFollowing, clearData },
     dispatch
   );
 
