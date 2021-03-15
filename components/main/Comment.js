93582e0eb073544b131cad6b875d83586d525cb6
Krisztian Holub <kholub1989@gmail.com>
2021-03-15 15:25:39 -0400

    create comment screen

---
+++ components/main/Comment.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import { View, Text, FlatList, Button, TextInput } from "react-native";
+
+import firebase from "firebase";
+require("firebase/firestore");
+
+export default function Comment(props) {
+  const [comments, setComments] = useState([]);
+  const [postId, setPostId] = useState("");
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    if (props.route.params.postId !== postId) {
+      firebase
+        .firestore()
+        .collection("post")
+        .doc(props.route.params.uid)
+        .collection("userPosts")
+        .doc(props.route.params.postId)
+        .collection("comments")
+        .get()
+        .then((snapshot) => {
+          let comments = snapshot.docs.map((doc) => {
+            const data = doc.data();
+            const id = doc.id;
+            return { id, ...data };
+          });
+          setComments(comments);
+        });
+      setPostId(props.route.params.postId);
+    }
+  }, [props.route.params.postId]);
+
+  return (
+    <View>
+      <FlatList
+        numColumns={1}
+        horizontal={false}
+        data={comments}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.text}</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
+}
