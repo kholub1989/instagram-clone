f58b73c09e2a8e979ccbc476eebb7144ce6a2994
Krisztian Holub <kholub1989@gmail.com>
2021-03-15 15:26:19 -0400

    add navigation to comment

--- components/main/Feed.js
+++ components/main/Feed.js
@@ -36,6 +36,16 @@ function Feed(props) {
             <View style={styles.containerImage}>
               <Text style={styles.container}>{item.user.name}</Text>
               <Image style={styles.image} source={{ uri: item.downloadURL }} />
+              <Text
+                onPress={() =>
+                  props.navigation.navigate("Comment", {
+                    postId: item.id,
+                    uid: item.user.uid,
+                  })
+                }
+              >
+                View Comments...
+              </Text>
             </View>
           )}
         />
